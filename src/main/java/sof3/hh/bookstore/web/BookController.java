933efe26374491f3fa764e66c65de89f498e583e
Juh1s <juho.koivumaa@aalto.fi>
2024-03-03 21:57:25 +0000

    5.1 Customized Spring REST

--- src/main/java/sof3/hh/bookstore/web/BookController.java
+++ src/main/java/sof3/hh/bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package sof3.hh.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import sof3.hh.bookstore.domain.Book;
 import sof3.hh.bookstore.domain.BookRepository;
@@ -37,6 +41,17 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";  // booklist.html
     }
+
+    // näytä kaikki kirjat
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {
+        return (List<Book>) bookRepository.findAll();
+    }
+    
+    @RequestMapping(value="/books/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return bookRepository.findById(bookId);
+    }      
 
     // tyhjän kirjalomakkeen muodostaminen
     @RequestMapping(value = "/addbook", method = RequestMethod.GET)
